a32a554afa9ab3f418274a4f7879406759d56d5c
pAdre91 <petr.osipov91@mail.ru>
2021-02-01 18:21:20 +0700

    Puting object in bag

---
+++ RAV3-Inc/Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+	private List<Item> _itemsInInventory = new List<Item>();
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		PutItem(collision.gameObject.GetComponent<InventoryItem>());
+	}
+
+	private void PutItem(InventoryItem inputItem)
+	{
+		if (inputItem == null)
+			return;
+
+		_itemsInInventory.Add(inputItem.Item);
+		Destroy(inputItem.gameObject);
+	}
+}
--- RAV3-Inc/Assets/Scripts/Item.cs
+++ RAV3-Inc/Assets/Scripts/Item.cs
@@ -3,11 +3,20 @@
 [CreateAssetMenu(fileName = "New Item", menuName = "Item/New Item")]
 public class Item : ScriptableObject
 {
+	[SerializeField] private GameObject _prefab;
+	[Space]
 	[SerializeField] private int _id = -1;
 	[SerializeField] private string _itemName = "DefaultItemName";
 	[SerializeField] private float _weight = -1;
-	[SerializeField] private ItemTypes _ = ItemTypes.Default;
+	[SerializeField] private ItemTypes _itemType = ItemTypes.Default;
+	[SerializeField] private Sprite _icon;
 
+	public GameObject Prefab => _prefab;
+	public int Id => _id;
+	public string ItemName => _itemName;
+	public float Weight => _weight;
+	public ItemTypes ItemType => _itemType;
+	public Sprite Icon => _icon;
 }
 
 public enum ItemTypes
